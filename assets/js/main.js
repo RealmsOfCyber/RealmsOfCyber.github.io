51b1504d50b01061aaeba116514ae28549632c52
zinduolis <ilgakulnis@gmail.com>
2024-11-25 14:52:11 +1000

    Incorporate feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Aug 28, 2025 09:00:00").getTime();
+var target_date = new Date("Aug 1, 2025 09:00:00").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;

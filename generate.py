a9882286c4befcac68866cc5ca8199eeed4f4c9a
Colby Prior <colby.prior@octopus.com>
2025-04-03 19:20:08 +1000

    init templating

---
+++ generate.py
@@ -0,0 +1,17 @@
+from jinja2 import Template, Environment, FileSystemLoader
+from variables import *
+
+# load templates folder to environment (security measure)
+env = Environment(loader=FileSystemLoader('templates'))
+
+# load the `index.jinja` template
+index_template = env.get_template('index.jinja')
+output_from_parsed_template = index_template.render(
+  year=year,
+  become_a_sponsor_url=become_a_sponsor_url,
+  sponsors=sponsors
+)
+
+# write the parsed template
+with open("site/index.html", "w") as chap_page:
+  chap_page.write(output_from_parsed_template)

e8f42bd0b9234c085ccf5012c02e525fcfa5a7cf
Colby Prior <cheesecolby7@gmail.com>
2025-04-08 06:31:35 +1000

    Update sponsor prospect url

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 year = 2025
-become_a_sponsor_url = "https://forms.office.com/r/HhTkHL5GV1"
+become_a_sponsor_url = "https://forms.office.com/r/hmGwh4tz4z"
 sponsors = {
     "Platinum": [
         {

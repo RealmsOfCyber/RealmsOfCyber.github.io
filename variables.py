d6c12e7a420006494848e917c16c7c1aa848ba42
Coopdogg24 <cdowiebridges@gmail.com>
2025-06-11 11:08:33 +1000

    Update sponsor blurb

--- variables.py
+++ variables.py
@@ -6,4 +6,4 @@ ticket_url = "https://events.humanitix.com/realms-of-cyber-2025/tickets"
 ticket_price = 1100
 become_a_sponsor_url = "https://forms.office.com/r/K5Tqfz2bq7"
 #sponsor_blurb = "We extend our deepest gratitude to the sponsors and partners who made ROCCON 2024 possible. Your generous support ensured the success of this year’s event and helped us bring together a community dedicated to advancing cybersecurity. Thank you to our Platinum sponsor, Queensland Government, as well as our Gold, Silver, and Bronze sponsors."
-sponsor_blurb = "Our generous sponsors support ensures the success of ROCCON to help us bring together a community dedicated to advancing cybersecurity."
+sponsor_blurb = "ROCC is proudly supported by the following sponsors driving Australia’s cybersecurity future"

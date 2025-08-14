14ee85c4c2fe5c06ff48b9c490cb732fde03cb00
Coopdogg24 <cdowiebridges@gmail.com>
2025-08-14 11:58:15 +1000

    Swap positioning of Speakers

--- speakers.py
+++ speakers.py
@@ -24,12 +24,12 @@ speakers = [
         "description": "Mike is the Founder and CEO of DroneSec, a UAS adversary intelligence company. DroneSec was founded in 2016 as an intelligence-led approach to C-UAS and serves FVEY military, government and law enforcement. Mike provides awareness of emerging UAS tactics, techniques and procedures in the rapidly scaling small UAS threat environment. Mike has been invited to present on drone threat proliferation at the United Nations, NATO and to the US National Security Council at The White House."
     },
     {
-        "name": "Bradley Butler",
-        "jobtitle": "Principal Security Consultant",
-        "company": "Mantel Group",
-        "linkedin": "https://www.linkedin.com/in/bradley-butler-msc-9445a154/",
-        "image": "bradley_butler.jpg",
-        "description": "An experienced principal security consultant competent in information governance, risk management, audit and compliance with recent experience consulting to the Australian government, commercial organisations, financial services, transport, manufacturing, and critical national infrastructure (CNI) industries. I have an MSc in Security and Risk Management and my military background in the Royal Marines provides a further level of management and security experience. I am knowledgeable and certified to a number of internationally recognised security standards and frameworks, including: ISO/IEC 27001:2013 and 22301, Australian Signals Directorate (ASD) Essential Eight, CIS Controls, ISA 62443, NIST CSF, and Special Publication 800 - 53.<br><br>Led multiple threat modelling and cyber security engagements against NIST CSF, ISO/IEC 27001:2013, ASD Essential Eight, APRA CPS 234, and other recognised standards; developed multiple strategic cyber security improvement roadmaps; presented strategic initiatives for cyber security improvement to C-suite stakeholders; delivered and led large scale supplier assurance programs for financial organisations; delivered cyber security assessments in industrial control systems (ICS) and operational technology (OT) domains; as a virtual information security manager, delivered security risk and audit services to multiple organisations; developed cyber security incident management plans and playbooks for multiple clients in the government sector; and delivered onsite security incident table-top exercises using tailored scenarios."
+        "name": "Ben Doyle",
+        "jobtitle": "Former Thales CISO",
+        "company": "",
+        "linkedin": "https://www.linkedin.com/in/bendoylethales/",
+        "image": "ben_doyle.jpg",
+        "description": "Ben Doyle is a seasoned cyber security leader who served as CISO for Thales Australia & New Zealand for over two decades. With more than 25 years in the industry, Ben has been at the helm of information security strategies that span national security obligations, export controls, and the evolving cyber threat landscape.<br><br>Now leading Cyber Security Industry Experience at Thales Cyber and Digital, Ben continues to influence industry thinking by mentoring startups, advising boards, and supporting innovation across Australia’s cyber ecosystem. He is a vocal advocate for values-led leadership and inclusive community culture, and an active contributor to advisory boards such as AISA and AustCyber."
     },
     {
         "name": "Michael Creagh",
@@ -104,12 +104,12 @@ speakers = [
         "description": "Gaurav is a cybersecurity leader who helps complex organisations move fast without compromising trust. With 16+ years across financial services, government, critical infrastructure, and tech, he’s built and led security programs that protect growth, recover from crisis, and hold under pressure - across Australia, the UK, and Asia.<br><br>He currently leads security for Axon’s Asia-Pacific region, working across public safety technologies and mission-critical platforms. He also advises selected organisations on scaling security capability, aligning with global compliance frameworks, and using security maturity as a lever to unlock new business and market expansion."
     },
     {
-        "name": "Ben Doyle",
-        "jobtitle": "Former Thales CISO",
-        "company": "",
-        "linkedin": "https://www.linkedin.com/in/bendoylethales/",
-        "image": "ben_doyle.jpg",
-        "description": "Ben Doyle is a seasoned cyber security leader who served as CISO for Thales Australia & New Zealand for over two decades. With more than 25 years in the industry, Ben has been at the helm of information security strategies that span national security obligations, export controls, and the evolving cyber threat landscape.<br><br>Now leading Cyber Security Industry Experience at Thales Cyber and Digital, Ben continues to influence industry thinking by mentoring startups, advising boards, and supporting innovation across Australia’s cyber ecosystem. He is a vocal advocate for values-led leadership and inclusive community culture, and an active contributor to advisory boards such as AISA and AustCyber."
+        "name": "Bradley Butler",
+        "jobtitle": "Principal Security Consultant",
+        "company": "Mantel Group",
+        "linkedin": "https://www.linkedin.com/in/bradley-butler-msc-9445a154/",
+        "image": "bradley_butler.jpg",
+        "description": "An experienced principal security consultant competent in information governance, risk management, audit and compliance with recent experience consulting to the Australian government, commercial organisations, financial services, transport, manufacturing, and critical national infrastructure (CNI) industries. I have an MSc in Security and Risk Management and my military background in the Royal Marines provides a further level of management and security experience. I am knowledgeable and certified to a number of internationally recognised security standards and frameworks, including: ISO/IEC 27001:2013 and 22301, Australian Signals Directorate (ASD) Essential Eight, CIS Controls, ISA 62443, NIST CSF, and Special Publication 800 - 53.<br><br>Led multiple threat modelling and cyber security engagements against NIST CSF, ISO/IEC 27001:2013, ASD Essential Eight, APRA CPS 234, and other recognised standards; developed multiple strategic cyber security improvement roadmaps; presented strategic initiatives for cyber security improvement to C-suite stakeholders; delivered and led large scale supplier assurance programs for financial organisations; delivered cyber security assessments in industrial control systems (ICS) and operational technology (OT) domains; as a virtual information security manager, delivered security risk and audit services to multiple organisations; developed cyber security incident management plans and playbooks for multiple clients in the government sector; and delivered onsite security incident table-top exercises using tailored scenarios."
     },
     {
         "name": "Jake Smith",
